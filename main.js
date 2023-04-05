ac73c8c5affe90eb0cf5e4bf2d84fbf83ca1f631
Enzzzzz <contatoenzz@gmail.com>
2023-04-05 13:45:26 -0300

    removed default nav options, added file/quit option on top

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 
 const isWin = process.platform === 'win32'
 
+//CREATE MAIN WINDOW
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Yulbot Resizer',
@@ -14,8 +15,14 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './render/index.html'));
 }
 
+//APP READY
 app.whenReady().then(() => {
     createMainWindow()
+
+    // MENU
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -23,6 +30,20 @@ app.whenReady().then(() => {
         }
     })
 })
+
+// MENU
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+                accellerator: 'Ctrl+W'
+            }
+        ]
+    }
+]
 
 app.on('window-all-closed', () => {
     if (!isWin) {
